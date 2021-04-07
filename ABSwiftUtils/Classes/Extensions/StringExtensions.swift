12572efe161170c6c7ee68ff41333a2a9bdef3c6
Aiden <aiden.jobdev@gmail.com>
2021-04-07 13:48:12 +0900

    Add property to `StringExtensions` for whether the email format is correct

--- ABSwiftUtils/Classes/Extensions/StringExtensions.swift
+++ ABSwiftUtils/Classes/Extensions/StringExtensions.swift
@@ -20,5 +20,19 @@ extension String {
 }
 
 
+// MARK: - Validation
+extension String {
+
+    public var isEmail: Bool {
+        return matches(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}")
+    }
+
+    func matches(pattern: String) -> Bool {
+        return range(of: pattern, options: String.CompareOptions.regularExpression, range: nil, locale: nil) != nil
+    }
+
+}
+
+
 #endif
 
